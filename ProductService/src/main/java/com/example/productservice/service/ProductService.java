b91d62b3473d5ae2f4566ab70950ebe584d7c7ff
TDMWalpala <tharindudarshana2019@gmail.com>
2023-11-08 17:56:50 +0530

    Product service implemented

---
+++ ProductService/src/main/java/com/example/productservice/service/ProductService.java
@@ -0,0 +1,45 @@
+package com.example.productservice.service;
+
+import com.example.productservice.dto.ProductRequest;
+import com.example.productservice.dto.ProductResponse;
+import com.example.productservice.model.Product;
+import com.example.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
+public class ProductService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private final ProductRepository productRepository;
+
+    public  void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product "+product.getId()+" is saved");
+
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        return modelMapper.map(products,new TypeToken<ArrayList<ProductResponse>>(){}.getType());
+    }
+}
